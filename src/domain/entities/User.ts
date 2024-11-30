35b510c4cd7d4ee62db388afc458a58232fd2ed0
Edson <biduzao.bidu21@gmail.com>
2024-11-29 22:56:51 -0300

    chore: modify user entity

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -5,26 +5,39 @@ export class User {
   constructor(
     public id: string,
     public email: string,
-    private _password: string, // Senha privada, que será armazenada como hash
-    public createdAt: Date = new Date(),
-    public updatedAt: Date = new Date()
+    private password: string, // Senha privada, que será armazenada como hash
+    public createdAt: Date,
+    public updatedAt?: Date
   ) {}
 
+  user(
+    id: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+    updatedAt?: Date
+  ) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    createdAt = createdAt;
+    updatedAt = updatedAt;
+  }
   // Getter para acessar a senha de forma segura (em hash)
-  get password(): string {
-    return this._password;
+  getpassword(): string {
+    return this.password;
   }
 
   // Método para definir a senha com hashing
   async setPassword(password: string): Promise<void> {
     const saltRounds = 10;
-    this._password = await bcrypt.hash(password, saltRounds); // Aplica o hash à senha
+    this.password = await bcrypt.hash(password, saltRounds); // Aplica o hash à senha
     this.updatedAt = new Date(); // Atualiza a data de modificação
   }
 
   // Método para verificar a senha (ao tentar logar, por exemplo)
   async comparePassword(password: string): Promise<boolean> {
-    return bcrypt.compare(password, this._password); // Compara o hash da senha fornecida com o hash armazenado
+    return bcrypt.compare(password, this.password); // Compara o hash da senha fornecida com o hash armazenado
   }
 
   // Método para atualizar o e-mail e a senha do usuário
