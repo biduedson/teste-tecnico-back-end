0a2d6370beeb83599c745887dd1ad07fe08ac7c7
Edson <biduzao.bidu21@gmail.com>
2024-11-29 22:57:56 -0300

    feat: create initial useCAses am useCAses impl

---
+++ src/aplication/interfaces/useCaseImpl/user/IUserUseCases.ts
@@ -0,0 +1,8 @@
+import { UserDTO } from "../../../dtos/userDTO";
+import { IUser } from "../../user/IUser";
+
+export interface IUserUseCases {
+  create(userDTO: UserDTO): Promise<IUser>;
+  update(id: string, email?: string, password?: string): Promise<IUser>;
+  delete(id: string): Promise<void>;
+}
