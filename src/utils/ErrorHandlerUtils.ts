94d104e1f777b9309844ece9cef36ae3d43cc687
Edson <biduzao.bidu21@gmail.com>
2024-11-30 11:38:03 -0300

    feat: create all handler error controllers

---
+++ src/utils/ErrorHandlerUtils.ts
@@ -0,0 +1,21 @@
+import { IHttpResponse } from "../aplication/interfaces/http/IHttpResponse";
+import { AlreadyExistsError } from "../domain/exeptions/AlreadyExistsError";
+import { BadRequestError } from "../domain/exeptions/BadRequestError";
+import { NotFoundError } from "../domain/exeptions/NotFoundError";
+
+export const errorHandlerUtils = (error: Error): IHttpResponse<any> => {
+  if (
+    error instanceof AlreadyExistsError ||
+    error instanceof BadRequestError ||
+    error instanceof NotFoundError
+  ) {
+    return {
+      statusCode: error.statusCode,
+      body: error.message,
+    };
+  }
+  return {
+    statusCode: 500,
+    body: `Erro interno do servidor, erro: ${error.message}`,
+  };
+};
